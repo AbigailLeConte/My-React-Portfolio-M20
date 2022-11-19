da73e152dbe9152245cce3da4b67e4929ed97e7c
Abigail LeConte <abigailleconte@Abigails-MBP.attlocal.net>
2022-11-18 19:09:51 -0500

    updated code

--- develop/src/App.js
+++ develop/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PortfolioContainer from "./components/PortfolioContainer";
 
 function App() {
   return (
@@ -18,8 +19,21 @@ function App() {
           Learn React
         </a>
       </header>
+      <PortfolioContainer />
     </div>
   );
 }
 
 export default App;
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(9000);
--- develop/src/index.js
+++ develop/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+ReactDOM.render(<App />, document.getElementById('root'));
+// I might not need line 8
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
